0797285a599ce2fc91c03a3811a231dba95e1fdc
Cristian21395 <Cristian.Gomez2@alu.uclm.es>
2018-01-27 23:46:22 +0100

    Cup fixed

--- src/jflex_cup/ContainerST.java
+++ src/jflex_cup/ContainerST.java
@@ -1,23 +1,22 @@
 import org.stringtemplate.v4.*;
 import java.util.LinkedList;
-import java.util.Queue;
 
 public class ContainerST{
-	private Queue<ST> qe;
+	private LinkedList<ST> list;
 
 	public ContainerST(){
-		this.qe = new LinkedList<ST>();
+		this.list = new LinkedList<ST>();
 	}
 
 	public void add(ST template){
-		qe.add(template);
+		list.add(template);
 	}
 
 	public ST poll(){
-		return qe.poll();
+		return list.removeLast();
 	}
 
 	public boolean isEmpty(){
-		return qe.peek() == null;
+		return list.size() == 0;
 	}
 }
